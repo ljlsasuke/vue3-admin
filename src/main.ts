15a0463d6ad4c7c895a1d441fb9335cf690bb909
ljlsasuke <ljlsasuke@foxmail.com>
2024-04-15 08:07:23 +0800

    更改element plus配置为中文

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ import { useAllDirective } from "@/directive";
 import App from "./App.vue";
 import "virtual:svg-icons-register";
 import "element-plus/theme-chalk/dark/css-vars.css";
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 const app = createApp(App);
 useAllDirective(app);
 // 注册全局组件
 app.use(globalComponents);
-app.use(ElementPlus);
+app.use(ElementPlus, { locale: zhCn });
 app.use(pinia);
 app.use(router);
 app.mount("#app");
